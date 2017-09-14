95de0514570ce3a913857237cb2eb795e798aa56
Levy <Carmel.Levy@emc.com>
2017-09-14 18:16:21 +0300

    Uploading initial project

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+
+        
+        
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 800);
+            this.Name = "Form1";
+            this.Text = "QR Code";
+            this.ResumeLayout(false);
+
+            int[,] initial_rep =   {   {1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+                                       {1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1},
+                                       {1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1},
+                                       {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1},
+                                       {1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1},
+                                       {1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+                                       {1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+                                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                                       {1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                                       {0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+                                       {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1},
+                                       {0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1},
+                                       {0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1},
+                                       {1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                                       {1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0},
+                                       {1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1},
+                                       {1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0},
+                                       {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0},
+                                       {1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1},
+                                       {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1},
+                                       {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1},
+                                       {1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0},
+                                       {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1},
+                                       {1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1},
+                                       {1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1}    };
+
+            rep = initial_rep;
+
+            ToolTip tt = new ToolTip();
+
+
+            for (int i = 0; i < 25; i++)
+            {
+                char c = 'A';
+
+                for (int j = 0; j < 25; j++)
+                {
+                    classButton newButton = new classButton();
+                    newButton.Location = new System.Drawing.Point(10 + j *30, 10 + i*30);
+                    newButton.Size = new System.Drawing.Size(30, 30);
+                    
+                    tt.SetToolTip(newButton, c.ToString() + (i + 1));
+                    
+                    //TODO : Decide how to show Text
+                    newButton.Text = c.ToString() + (i + 1);
+                    //newButton.MouseEnter += 
+                    // newButton.MouseLeave +=
+
+                    newButton.Font = new Font(newButton.Font.FontFamily, 6);
+                    newButton.TabStop = false;
+                    newButton.FlatStyle = FlatStyle.Flat;
+                    newButton.FlatAppearance.BorderSize = 0;
+                    if (rep[i, j] == 1)
+                    {
+                        newButton.BackColor = System.Drawing.Color.Black;
+                    }
+                    else
+                    {
+                        newButton.BackColor = System.Drawing.Color.White;
+                    }
+                    newButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(0, 255, 255, 255);
+                    newButton.Click += b_Click;
+
+                    
+                    this.Controls.Add(newButton);
+                    QRmat[i,j] = newButton;
+
+                    c++;
+                }
+            }
+        }
+
+        #endregion
+
+        private int[,] rep;
+
+        private Button[,] QRmat = new Button[25,25];
+
+        public class classButton : Button
+        {
+            public string strHiddenText;
+
+            public string HiddenText
+            {
+                get { return strHiddenText; }
+                set { strHiddenText = value; }
+            }
+
+            public void hideText() { Text = ""; }
+            public void showText() { Text = strHiddenText; }
+        }
+
+       
+        void b_Click(object sender, System.EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.BackColor == System.Drawing.Color.White)
+                b.BackColor = System.Drawing.Color.Black;
+            else
+                b.BackColor = System.Drawing.Color.White;
+        }
+
+
+    }
+
+}
+
